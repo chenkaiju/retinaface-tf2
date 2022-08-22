fix decode_tf bug by adding normalization

modules/anchor2.py
@@ -253,11 +253,12 @@ def _jaccard(box_a, box_b):
 def decode_tf(labels, priors, variances=[0.1, 0.2]):
     """tensorflow decoding"""
     bbox = _decode_bbox(labels[:, :4], priors, variances)
-    landm = _decode_landm(labels[:, 4:14], priors, variances)
-    landm_valid = labels[:, 14][:, tf.newaxis]
-    conf = labels[:, 15][:, tf.newaxis]
+    landm = _decode_landm(labels[:, 4:4+136], priors, variances)
+    param = labels[:, 4+136:4+136+62]
+    landm_valid = labels[:, -2][:, tf.newaxis]
+    conf = labels[:, -1][:, tf.newaxis]
 
-    return tf.concat([bbox, landm, landm_valid, conf], axis=1)
+    return tf.concat([bbox, landm, param, landm_valid, conf], axis=1)
 
 
 def _decode_bbox(pre, priors, variances=[0.1, 0.2]):
@@ -290,10 +291,10 @@ def _decode_landm(pre, priors, variances=[0.1, 0.2]):
     Return:
         decoded landm predictions
     """
-    landms = tf.concat(
-        [priors[:, :2] + pre[:, :2] * variances[0] * priors[:, 2:],
-         priors[:, :2] + pre[:, 2:4] * variances[0] * priors[:, 2:],
-         priors[:, :2] + pre[:, 4:6] * variances[0] * priors[:, 2:],
-         priors[:, :2] + pre[:, 6:8] * variances[0] * priors[:, 2:],
-         priors[:, :2] + pre[:, 8:10] * variances[0] * priors[:, 2:]], axis=1)
+    landms = []
+    for i in range(0, pre.shape[1], 2):
+        landms.append(priors[:, :2] + pre[:, i:i+2] * variances[0] * priors[:, 2:])
+    
+    landms = tf.concat(landms, axis=1)
+    
     return landms

modules/dataset2.py
@@ -54,14 +54,14 @@ def get_facebox2d(landmark_2d, ratio=0.1):
     return facebox
 
 def _parse_tfds(bfm, img_dim, priors, match_thresh, 
-                ignore_thresh, variances, numFace=2):
+                ignore_thresh, variances, numFace=1):
     
     def parse_tfds(dataset):
         
         labels = []
         image = dataset['image']
-        
-        for i in range(numFace):
+
+        for n in range(numFace):
             label = []
             param = dataset['param']
             
@@ -70,13 +70,15 @@ def _parse_tfds(bfm, img_dim, priors, match_thresh,
             
             facebox = tf.squeeze(get_facebox2d(lmk_2d))
             
+            # stack and normalize facebox [0, 1]
             for i in range(facebox.shape[0]):
-                label.append(facebox[i])
-                
+                label.append(facebox[i] / img_dim)
+            
+            # stack and normalize lmk [0, 1]
             lmk_2d = tf.transpose(tf.squeeze(lmk_2d))
             for i in range(lmk_2d.shape[0]):
                 for j in range(lmk_2d.shape[1]):
-                    label.append(lmk_2d[i][j])
+                    label.append(lmk_2d[i][j] / img_dim)
             
             for p in param:
                 label.append(p)